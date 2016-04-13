99fe23dcbcd1f6bd00a83f235fdde2953ef86843
Fabio R <fabio@ribei.ro>
2016-04-13 19:54:40 -0400

    facebook integration using node

--- node/bot_fb/bot.js
+++ node/bot_fb/bot.js
@@ -36,22 +36,24 @@ app.post('/bot/facebook', function(req, res) {
   // Process the Facebook updates here
 
    messaging_events = req.body.entry[0].messaging;
+
 	console.log(JSON.stringify(messaging_events));
 
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
     sender = event.sender.id;
+    
     if (event.message && event.message.text) {
       text = event.message.text;
       
-     	console.log(text);
+     	// console.log(text);
 
       // Handle a text message from this sender
 
 
     //  sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
     }
-      		sendTextMessage(sender, "Text received");
+      		// sendTextMessage(sender, "Text received");
 
   }
 
@@ -89,6 +91,3 @@ function sendTextMessage(sender, text) {
   console.log("sent ");
 }
 
-
-
-app.listen();
