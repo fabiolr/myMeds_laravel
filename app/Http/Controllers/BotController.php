7ab8e827eb63d48a08a6e31a967f468cfa8cfd12
Fabio R <fabio@ribei.ro>
2016-04-12 23:06:58 -0400

    debugging facebook api

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -10,7 +10,7 @@ class BotController extends Controller
 {
 		public function callback(Request $request) {    
 
-		    return $request->input('hub.challenge');
+		    return $request->input("hub.challenge");
 
 		    // if ($request["hub.verify_token"] == "Oquc1jcCyHOw3TQtZBOJYt3TcO") {
 
