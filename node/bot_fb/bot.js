3d53b5d94b85bbec5cb2eb8d92f76d15eb582c65
Fabio R <fabio@ribei.ro>
2016-04-14 15:56:01 -0400

    wit integration

--- node/bot_fb/bot.js
+++ node/bot_fb/bot.js
@@ -9,19 +9,50 @@ const logger = new Logger(levels.DEBUG);
 var context = {};
 var sessions = {};
 
+
+const allJokes = {
+  chuck: [
+    'Chuck Norris counted to infinity - twice.',
+    'Death once had a near-Chuck Norris experience.',
+  ],
+  tech: [
+    'Did you hear about the two antennas that got married? The ceremony was long and boring, but the reception was great!',
+    'Why do geeks mistake Halloween and Christmas? Because Oct 31 === Dec 25.',
+  ],
+  default: [
+    'Why was the Math book sad? Because it had so many problems.',
+  ],
+};
+
+
 const actions = {
   say: (sessionId, msg, cb) => {
     console.log(msg);
     cb();
   },
   merge: (context, entities, cb) => {
+    delete context.joke;
+    const category = firstEntityValue(entities, 'category');
+    if (category) {
+      context.cat = category;
+    }
+    const sentiment = firstEntityValue(entities, 'sentiment');
+    if (sentiment) {
+      context.ack = sentiment === 'positive' ? 'Glad you liked it.' : 'Hmm.';
+    } else {
+      delete context.ack;
+    }
     cb(context);
   },
-  error: (sessionid, msg) => {
+  error: (sessionId, msg) => {
     console.log('Oops, I don\'t know what to do.');
   },
+  'select-joke': (context, cb) => {
+    const jokes = allJokes[context.cat || 'default'];
+    context.joke = jokes[Math.floor(Math.random() * jokes.length)];
+    cb(context);
+  },
 };
-
 
 var bodyParser = require('body-parser');
 var express = require('express');
