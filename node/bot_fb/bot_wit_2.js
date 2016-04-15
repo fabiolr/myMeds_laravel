263aeff18c768032da207b397ca8e498716bd3cb
Fabio R <fabio@ribei.ro>
2016-04-14 20:22:31 -0400

    wit integration

--- node/bot_fb/bot_wit_2.js
+++ node/bot_fb/bot_wit_2.js
@@ -1,5 +1,12 @@
 
 var wit = require('node-wit');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'));
+
+
  
 var ACCESS_TOKEN = "MD3ZNDB5MEGX7KLS7Y24FGQBJXBOFQWO";
 wit.captureTextIntent(ACCESS_TOKEN, "What's the weather in Melbourne?", function (err, res) {
