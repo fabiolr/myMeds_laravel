1f5cc1b4c918cd2ee8c5c302f2266afc1305931f
Fabio R <fabio@ribei.ro>
2016-04-16 19:31:16 -0400

    implementing wit to the working fb interaction

--- node/bot_fb/bot_wit.js
+++ node/bot_fb/bot_wit.js
@@ -20,22 +20,22 @@ const wit_token = "YTBUDDOE7QONDWDBW4PBDTHXXR3JS4BV"; //quotes
 
 const sessions = {};
 
-// const findOrCreateSession = (fbid) => {
-//   var sessionId;
-//   // Let's see if we already have a session for the user fbid
-//   Object.keys(sessions).forEach(k => {
-//     if (sessions[k].fbid === fbid) {
-//       // Yep, got it!
-//       sessionId = k;
-//     }
-//   });
-//   if (!sessionId) {
-//     // No session found for user fbid, let's create a new one
-//     sessionId = new Date().toISOString();
-//     sessions[sessionId] = {fbid: fbid, context: {}};
-//   }
-//   return sessionId;
-// };
+const findOrCreateSession = (fbid) => {
+  var sessionId;
+  // Let's see if we already have a session for the user fbid
+  Object.keys(sessions).forEach(k => {
+    if (sessions[k].fbid === fbid) {
+      // Yep, got it!
+      sessionId = k;
+    }
+  });
+  if (!sessionId) {
+    // No session found for user fbid, let's create a new one
+    sessionId = new Date().toISOString();
+    sessions[sessionId] = {fbid: fbid, context: {}};
+  }
+  return sessionId;
+};
 
 
 
