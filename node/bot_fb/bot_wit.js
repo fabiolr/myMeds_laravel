36568e2a61954b7eb9669f22b079a0142fbdb553
Fabio R <fabio@ribei.ro>
2016-04-17 00:18:28 -0400

    implementing wit to the working fb interaction

--- node/bot_fb/bot_wit.js
+++ node/bot_fb/bot_wit.js
@@ -1,5 +1,6 @@
 
-var fb_token = "CAAIsNqBHnnkBAPzfc9krR6V6ZB0X81ZCr56nLakZAOD3wyTpTMhYBLZA7XePyMxRG8O3laI1DCuPkrGcSdWzzq3VZCGvzynBGQIUiAxk1Bpcp1z5jC0J4XmH2CwhnNaTZAYZCeXvZABFsZCSZAzZBv0rDT8sFBONYHFezZBeDsynLBbGebr9zRaxPrAKXPCVDZCYkDVlNjRJclAiiHQZDZD";
+require('dotenv').load();
+
 
 // used for aeverything
 var express = require('express');
@@ -14,8 +15,6 @@ var sender;
 // used for wit
 var request = require('request');
 const Wit = require('node-wit').Wit;
-const wit_token = "YTBUDDOE7QONDWDBW4PBDTHXXR3JS4BV"; //quotes
-// const token = "MD3ZNDB5MEGX7KLS7Y24FGQBJXBOFQWO"; //meds
 
 // fb sessions
 
@@ -192,7 +191,7 @@ function respondToUser(sender, msg, cb) {
   }
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:fb_token},
+    qs: {access_token:process.env.FB_TOKEN},
     method: 'POST',
     json: {
       recipient: {id:sender},
@@ -212,7 +211,7 @@ function respondToUser(sender, msg, cb) {
 app.listen();
 
 // wit method to deal with message
-const client = new Wit(wit_token, actions);
+const client = new Wit(process.env.WIT_TOKEN, actions);
 
 
 
