cbdfdef74beb39d1a9d7072b12d4ab21116075c6
Fabio R <fabio@ribei.ro>
2016-04-18 16:52:29 -0400

    bot can now respond about med uses

--- node/bot_fb/bot_wit.js
+++ node/bot_fb/bot_wit.js
@@ -87,7 +87,7 @@ const actions = {
      request('http://quotes.rest/qod.json?category=inspire', function (error, response, body) {
               if (!error && response.statusCode == 200) {
                 data = body;
-                context.selected = JSON.parse(body).contents.quotes[0].quote;
+                context.quote = JSON.parse(body).contents.quotes[0].quote;
                 return cb(context);
                 } else {
                   console.log('got an error from the quote api');
