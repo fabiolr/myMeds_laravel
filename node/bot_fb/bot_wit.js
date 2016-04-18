68890714ecffc949d10ec3c2a612380cf4c8913f
Fabio R <fabio@ribei.ro>
2016-04-18 13:27:33 -0400

    bot can now respond about med uses

--- node/bot_fb/bot_wit.js
+++ node/bot_fb/bot_wit.js
@@ -94,21 +94,25 @@ const actions = {
                 }
       })
   },
-    'getUse': (context, cb) => {
+  'getUse': (context, cb) => {
      
      if (context.drug) {
+      console.log('drug is: ' + context.drug);
 
      request('https://mymeds.miami/json/search/' + context.drug, function (error, response, body) {
               if (!error && response.statusCode == 200) {
                 context.use = JSON.parse(body).uses[0].use;
+                console.log(context.use)
+                //context.use = 'something';
                 return cb(context);
                 }
       })
    } else {
     console.log('oops i dont know what drug he meant');
-
+    return cb('error');
    }
   },
+
 };
 
 
