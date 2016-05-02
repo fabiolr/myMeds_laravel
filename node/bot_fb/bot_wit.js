a6b6c99a0bfcd9ed941263ea8647a3edd8f3a275
Fabio R <fabio@ribei.ro>
2016-05-02 11:30:38 -0400

    bot token

--- node/bot_fb/bot_wit.js
+++ node/bot_fb/bot_wit.js
@@ -202,7 +202,7 @@ app.post('/bot/facebook', function(req, res) {
   res.sendStatus(200);
 
 });
-
+CAAIsNqBHnnkBAPzfc9krR6V6ZB0X81ZCr56nLakZAOD3wyTpTMhYBLZA7XePyMxRG8O3laI1DCuPkrGcSdWzzq3VZCGvzynBGQIUiAxk1Bpcp1z5jC0J4XmH2CwhnNaTZAYZCeXvZABFsZCSZAzZBv0rDT8sFBONYHFezZBeDsynLBbGebr9zRaxPrAKXPCVDZCYkDVlNjRJclAiiHQZDZD
 
 // function to responde to user on facebook
 
@@ -215,7 +215,8 @@ function respondToUser(sender, msg, cb) {
   }
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:process.env.FB_TOKEN},
+    qs: {access_token:CAAIsNqBHnnkBAPzfc9krR6V6ZB0X81ZCr56nLakZAOD3wyTpTMhYBLZA7XePyMxRG8O3laI1DCuPkrGcSdWzzq3VZCGvzynBGQIUiAxk1Bpcp1z5jC0J4XmH2CwhnNaTZAYZCeXvZABFsZCSZAzZBv0rDT8sFBONYHFezZBeDsynLBbGebr9zRaxPrAKXPCVDZCYkDVlNjRJclAiiHQZDZD},
+    //qs: {access_token:process.env.FB_TOKEN},
     method: 'POST',
     json: {
       recipient: {id:sender},
